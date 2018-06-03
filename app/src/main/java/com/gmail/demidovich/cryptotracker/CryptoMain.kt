0fd037ef5a6d4fc6deae70424d216687244c9916
Markus-Demi <m4rk.demi@gmail.com>
2018-06-03 22:36:20 +0300

    Init

---
+++ app/src/main/java/com/gmail/demidovich/cryptotracker/CryptoMain.kt
@@ -0,0 +1,12 @@
+package com.gmail.demidovich.cryptotracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CryptoMain : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crypto_main)
+    }
+}
